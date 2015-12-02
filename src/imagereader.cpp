5a84dd82a4bcc5ac36b92bc50ecc66e92d7439ed
Voutilainen Kaisa <voutilk2@lyta.org.aalto.fi>
2015-12-02 19:00:23 +0200

    OCR works with the two screenshots in the src folder. Does not recognize Ä/Å/Ö.

--- src/imagereader.cpp
+++ src/imagereader.cpp
@@ -6,6 +6,12 @@
 #include <locale.h>
 #include "imagereader.h"
 
+#include <QString>
+#include <QFile>
+#include <QImage>
+#include <QPixmap>
+#include <QDir>
+
 ImageReader::ImageReader(QObject *parent) : QObject(parent)
 {
     qDebug() << "Init ImageReader";
@@ -14,30 +20,89 @@ ImageReader::ImageReader(QObject *parent) : QObject(parent)
 //Returns true if the filePath exists and can be read with Tesseract
 //Fills the Grid given as a reference
 bool ImageReader::initData(QString filePath, QVector<QVector<char>>& grid)
-{
+{    
+    //Create a vector for storing all 16 letters
+    QVector<QPixmap> letterList;
+    //Rescale the given image
+    QImage img(filePath);
+    QPixmap pm;
+    pm = pm.fromImage(img.scaled(384, 512, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+
+
+    //for-loop for cropping all letters from the image one by one
+    for(unsigned int i = 0; i < 16; i++){
+        //Create a rectangle for cropping each letter
+        QRect box(55+(i%4)*74, 199+(i/4)*73, 50, 50);
+        QPixmap letter = pm.copy(box);
+        //Add the pixmap of a letter to the vector for letters
+        letterList.append(letter);
+    }
+
+
+    //The file path where the letters will be saved before ocr-handling
+    QString newFile = "../src/OCRtemps/templetter.png";
+
+
+
     qDebug() << "ImageReader::initData(" << filePath << ", " << grid << ") - NOT IMPLEMENTED";
 
-    //OCR - BROKEN BUT YOU GET THE IDEA
+
+    /* OCR
+     * To get the needed file:
+     * wget https://tesseract-ocr.googlecode.com/files/eng.traineddata.gz
+     * gunzip eng.traineddata.gz
+     * sudo mv -v eng.traineddata /usr/share/tesseract-ocr/tessdata/
+     *
+     *
+     * */
+
+    //variable for storing the data from the cropped letter images
     char *outText;
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+
     // Initialize tesseract-ocr with English, setlocale to prevent crash from not finding tesseract (fix that?)
     setlocale (LC_NUMERIC, "C");
-    if (api->Init(NULL, "eng")) {
+    if (api->Init("usr/share/tesseract-ocr/tessdata/eng.traineddata", NULL)) {
         qDebug() << "Could not initialize tesseract.";
         return false;
     }
-    // Open input image with leptonica library
-    Pix *image = pixRead(filePath.toUtf8().constData());
-    api->SetImage(image);
+
+    api->SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
+    api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖ");
+
+    //string for the final grid
+    QString result;
+    Pix *image;
+    //for-loop for reading the letters
+    for(unsigned int t = 0; t < 16; t++){
+
+        //Save the letter as .png
+        QFile file(newFile);
+        if(!file.exists()){
+            //the file did not exist
+        } else {
+            file.open(QIODevice::WriteOnly);
+            letterList[t].save(newFile, "png");
+        }
+        file.close();
+        // Open input image with leptonica library
+        image = pixRead(newFile.toUtf8().constData());
+        api->SetImage(image);
+        outText = api->GetUTF8Text();
+        result += QString(outText).simplified().replace(" ", "");
+        qDebug() << result;
+    }
+
     // Get OCR result
-    outText = api->GetUTF8Text();
+
     qDebug() << "OCR from image:";
     qDebug() << outText;
-    QString result = QString(outText).simplified().replace(" ", "");
+
     qDebug() << "Parsed to QString:";
     qDebug() << result;
     int i = 0;
+
     //Save OCR-data in grid - HARDCODED 4X4, fix with a general purpose approach
     for(int x = 0; x < 4; x++) {
         for(int y = 0; y < 4; y++) {
