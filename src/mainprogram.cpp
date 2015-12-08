5b4c30f2d504da0e0eccb957f0ea5078f9427efa
Lassi Heikkinen <lassi.heikkinen@aalto.fi>
2015-12-08 16:55:45 +0200

    add drag n drop for text files

--- src/mainprogram.cpp
+++ src/mainprogram.cpp
@@ -1,5 +1,7 @@
 #include "mainprogram.h"
 #include <QDebug>
+#include <QFile>
+#include <QTextStream>
 
 MainProgram::MainProgram() : QObject()
 {
@@ -23,22 +25,48 @@ void MainProgram::makeGrid(QString filePath)
     QVector<QChar> empty(4, space);
     grid.fill(empty, 4);
     words.clear();
-    qDebug() << "MainProgram::makeGrid(" << filePath << ") - NOT IMPLEMENTED";
     qDebug() << "MainProgram::makeGrid(" << filePath << " )";
-    //Do different things based on the filetype
-    //Parse an image with Tesseract
-    qDebug() << "start OCR";
-    bool imageRead = imgReader->initData(filePath, grid);
-    qDebug() << "OCR finished";
-    if(imageRead) {
-        qDebug() << "OCR was successful";
+
+    QString ext = filePath.split(".").last().toLower();
+    QVector<QString> imagetypes = {"gif", "jpeg", "jpg", "png"};
+    if(imagetypes.contains(ext)) {
+        //Parse an image with Tesseract
+        qDebug() << "start OCR";
+        bool imageRead = imgReader->initData(filePath, grid);
+        qDebug() << "OCR finished";
+        if(imageRead) {
+            qDebug() << "OCR was successful";
+            solver->solve(grid, words);
+            qDebug() << "Solved: " << grid;
+            emit gridUpdated();
+        }
+    }
+    else if (ext == "txt") {
+        qDebug() << "reading to grid from text file";
+        QFile inputFile(filePath);
+        if (inputFile.open(QIODevice::ReadOnly)) {
+           QTextStream in(&inputFile);
+           for (int i = 0; i < 4; i++) {
+               if (in.atEnd()) {
+                   qDebug() << "file had insufficient lines";
+                   return;
+               }
+
+               QString line = in.readLine();
+               if (line.length() < 4) {
+                   qDebug() << "line " << i << " did not have enough characters";
+                   return;
+               }
+
+               for (int j = 0; j < 4; j++) {
+                   grid[i][j] = line[j];
+               }
+           }
+           inputFile.close();
+        }
+        else {qDebug() << "unable to open file " << filePath; return;}
+        qDebug() << "text file read successful";
         solver->solve(grid, words);
-        qDebug() << "Solved: " << grid;
         emit gridUpdated();
     }
-    //Or read from a text file
-    //???
-
-    //Then call Solver::solve
-
 }
